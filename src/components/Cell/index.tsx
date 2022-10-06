82dbd2d3701f3ce066aedb261a721992d6d3e423
artemkons <konstantinov050701@gmail.com>
2022-10-06 11:22:54 +0300

    Add SimpleEditor

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,16 +1,32 @@
-import SyntaxHighlighter from 'react-syntax-highlighter'
+import { useState } from 'react'
+
+import Editor from 'react-simple-code-editor';
+import Prism from 'prismjs'
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism.css';
 
 import './index.sass'
 
 const Cell = () => {
+    const [code, setCode] = useState('print("Hello world!")')
+
     return (
         <div className='cell'>
             <div className='cell__prompt'>
                 In [3]:
             </div>
-            <SyntaxHighlighter language='python'>
-                print(hello world)
-            </SyntaxHighlighter>
+            <Editor
+                value={code}
+                onValueChange={code => setCode(code)}
+                // TODO Find python syntax highlighter
+                highlight={code => Prism.highlight(code, Prism.languages.js, 'js')}
+                padding={10}
+                style={{
+                    fontFamily: '"Fira code", "Fira Mono", monospace',
+                    fontSize: 12,
+                }}
+            />
         </div>)
 }
 
